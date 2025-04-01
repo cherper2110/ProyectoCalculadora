584f99433555e851aea44e239eb0f46cd607325d
cherper2110 <bhp14100@gmail.com>
2025-04-01 14:29:03 +0200

    Update PruebaCalculadora.java

--- PruebaCalculadora.java
+++ PruebaCalculadora.java
@@ -1,9 +1,21 @@
 public class PruebaCalculadora {
     public static void main(String[] args) {
-        // Crear una instancia de la calculadora con dos números
+        // Crear una instancia de la clase Calculadora con dos números
         Calculadora calc = new Calculadora(10, 5);
-        
-        // Probar los métodos
+
+        // Probar la operación de suma
         System.out.println("Suma: " + calc.realizarOperacion("suma"));
+
+        // Probar la operación de resta
+        System.out.println("Resta: " + calc.realizarOperacion("resta"));
+
+        // Probar la operación de multiplicación
+        System.out.println("Multiplicación: " + calc.realizarOperacion("multiplicacion"));
+
+        // Probar la operación de división
+        System.out.println("División: " + calc.realizarOperacion("division"));
+
+        // Probar la operación de módulo
+        System.out.println("Módulo: " + calc.realizarOperacion("modulo"));
     }
 }
