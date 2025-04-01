abcdbcd3348cb346163e54b5c7e347b602e13a5c
cherper2110 <bhp14100@gmail.com>
2025-04-01 13:24:37 +0200

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -2,11 +2,23 @@ public class Calculadora {
     private int num1;
     private int num2;
 
+    // Constructor
     public Calculadora(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
     }
 
+    // Método para realizar una operación según la entrada
+    public int realizarOperacion(String operacion) {
+        if (operacion.equals("suma")) {
+            return suma();
+        } else {
+            System.out.println("Operación no soportada aún.");
+            return 0;
+        }
+    }
+
+    // Método de suma
     public int suma() {
         return num1 + num2;
     }
